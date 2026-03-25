8b035d0f5a7c03252b256d1322f15e486a4f16f9
Ramona Reinholdz <rare2400@student.miun.se>
2026-03-25 15:15:41 +0100

    lägger till kommentarer för förbättrad förståelse av kod

--- todo-list/src/components/TodoForm.tsx
+++ todo-list/src/components/TodoForm.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import "./TodoForm.css";
 
+// props definition får component with onAdd function from parent to create new todo
 interface Props {
     onAdd: (title: string, description: string) => void;
 }
 
 const TodoForm = ({ onAdd }: Props) => {
+    // state for forminputs and validation error messages
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [errors, setErrors] = useState<{ title?: string; description?: string }>({});
 
+    // validate form inputs
     const validate = () => {
         const newErrors: { title?: string; description?: string } = {};
         if (title.trim().length < 3) {
@@ -18,15 +21,25 @@ const TodoForm = ({ onAdd }: Props) => {
         if (description.length > 200) {
             newErrors.description = "Beskrivningen får max vara 200 tecken";
         }
+
+        // update error state
         setErrors(newErrors);
+
+        //return true if no errors exists
         return Object.keys(newErrors).length === 0;
     };
 
-    const handleSubmit = (e: React.SubmitEvent) => {
+    // handle the submit event
+    const handleSubmit = (e: any) => {
         e.preventDefault();
+
+        // stop submission if validation fails
         if (!validate()) return;
 
+        // call parent function to add todo
         onAdd(title.trim(), description.trim());
+
+        // reset form fields after successful submit, and clear error
         setTitle("");
         setDescription("");
         setErrors({});
@@ -38,11 +51,13 @@ const TodoForm = ({ onAdd }: Props) => {
                 <h2>Lägg till uppgift</h2>
                 <div className="form-group">
                     <label>Title *</label>
+                    {/* controlled input for title and show validation error if exists */}
                     <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Ange titel" />
                     {errors.title && <p className="error">{errors.title}</p>}
                 </div>
                 <div className="form-group">
                     <label>Beskrivning (valfri)</label>
+                    {/* controlled textarea for description and show validation error if exists */}
                     <textarea value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Ange beskrivning..." />
                     {errors.description && <span className="error">{errors.description}</span>}
                 </div>
